61183698c926571c0f042b786384474dbdd6485c
Alexa Hunleth <alexa@hunleth.com>
2019-09-22 20:36:15 -0400

    Make prettier math calls

--- lib/polyhedra/utilities.ex
+++ lib/polyhedra/utilities.ex
@@ -1,4 +1,6 @@
 defmodule Polyhedra.Utilities do
+  import :math
+
   def a_triangle(b, h) do
     b * h / 2
   end
@@ -6,7 +8,7 @@ defmodule Polyhedra.Utilities do
   def equilateral_triangle(b) do
     c = b
     a = b / 2
-    h = :math.sqrt(:math.pow(c, 2) - :math.pow(a, 2))
+    h = sqrt(pow(c, 2) - pow(a, 2))
     a_triangle(b, h)
   end
 
@@ -19,7 +21,7 @@ defmodule Polyhedra.Utilities do
   end
 
   def a_octagon(b) do
-    a = b / :math.sqrt(2)
+    a = b / sqrt(2)
     s = 2 * a + b
     c = s * s
     t = a * a / 2
@@ -28,11 +30,11 @@ defmodule Polyhedra.Utilities do
 
   def a_decagon(b) do
     n = b * b * 10
-    t = :math.tan(18)
+    t = tan(18)
     n / 4 * t
   end
 
   def find_height(b) do
-    :math.sqrt(b * b - b * b / 4)
+    sqrt(b * b - b * b / 4)
   end
 end
--- lib/polyhedra/volume.ex
+++ lib/polyhedra/volume.ex
@@ -1,12 +1,13 @@
 defmodule Polyhedra.Volume do
   ## Platonic Solids
+  import :math
 
   def cube(s) do
     s * s * s
   end
 
   def tetrahedron(b) do
-    :math.pow(b, 3) / 12 * :math.sqrt(2)
+    pow(b, 3) / 12 * sqrt(2)
   end
 
   def octahedron(b) do
@@ -14,90 +15,90 @@ defmodule Polyhedra.Volume do
   end
 
   def dodecahedron(b) do
-    n = 15 + 7 * :math.sqrt(5)
-    n / 4 * :math.pow(b, 3)
+    n = 15 + 7 * sqrt(5)
+    n / 4 * pow(b, 3)
   end
 
   def icosahedron(b) do
     a = 5 / 12
-    d = 3 + :math.sqrt(5)
-    c = :math.pow(b, 3)
+    d = 3 + sqrt(5)
+    c = pow(b, 3)
     a * d * c
   end
 
   ## Archimedean Solids
 
   def cubeoctahedron(b) do
-    5 / 3 * :math.sqrt(2) * :math.pow(b, 3)
+    5 / 3 * sqrt(2) * pow(b, 3)
   end
 
   def icosidodecahedron(b) do
-    c = 45 + 17 * :math.sqrt(5)
-    :math.pow(b, 3) / 6 * c
+    c = 45 + 17 * sqrt(5)
+    pow(b, 3) / 6 * c
   end
 
   def truncated_tetrahedron(b) do
-    23 / 12 * :math.sqrt(2) * :math.pow(b, 3)
+    23 / 12 * sqrt(2) * pow(b, 3)
   end
 
   def truncated_octahedron(b) do
-    8 * :math.pow(b, 3) * :math.sqrt(2)
+    8 * pow(b, 3) * sqrt(2)
   end
 
   def truncated_cube(b) do
-    c = 21 + 14 * :math.sqrt(2)
-    :math.pow(b, 3) / 3 * c
+    c = 21 + 14 * sqrt(2)
+    pow(b, 3) / 3 * c
   end
 
   def truncated_icosahedron(b) do
-    c = 125 + 43 * :math.sqrt(5)
-    :math.pow(b, 3) / 4 * c
+    c = 125 + 43 * sqrt(5)
+    pow(b, 3) / 4 * c
   end
 
   def truncated_dodecahedron(b) do
-    c = 99 + 47 * :math.sqrt(5)
-    5 / 12 * :math.pow(b, 3) * c
+    c = 99 + 47 * sqrt(5)
+    5 / 12 * pow(b, 3) * c
   end
 
   def small_rhombicuboctahedron(b) do
-    c = 6 + 5 * :math.sqrt(2)
-    2 / 3 * :math.pow(b, 3) * c
+    c = 6 + 5 * sqrt(2)
+    2 / 3 * pow(b, 3) * c
   end
 
   def great_rhombicuboctahedron(b) do
-    c = 22 + :math.sqrt(2)
-    c * :math.pow(b, 3)
+    c = 22 + sqrt(2)
+    c * pow(b, 3)
   end
 
   def small_rhombicosidodecahedron(b) do
-    c = 60 + 29 * :math.sqrt(5)
-    :math.pow(b, 3) / 3 * c
+    c = 60 + 29 * sqrt(5)
+    pow(b, 3) / 3 * c
   end
 
   def great_rhombicosidodecahedron(b) do
-    c = 95 + 50 * :math.sqrt(5)
-    c * :math.pow(b, 3)
+    c = 95 + 50 * sqrt(5)
+    c * pow(b, 3)
   end
 
   def snub_cube(b) do
     t =
-      (1 + :math.pow(19 + 3 * :math.sqrt(33), 1 / 3) + :math.pow(19 - 3 * :math.sqrt(33), 1 / 3)) /
+      (1 + pow(19 + 3 * sqrt(33), 1 / 3) + pow(19 - 3 * sqrt(33), 1 / 3)) /
         3
 
-    n = 3 * :math.sqrt(t - 1) + 4 * :math.sqrt(t + 1)
-    d = 3 * :math.sqrt(2 - t)
-    :math.pow(b, 3) * n / d
+    n = 3 * sqrt(t - 1) + 4 * sqrt(t + 1)
+    d = 3 * sqrt(2 - t)
+    pow(b, 3) * n / d
   end
 
   def snub_dodecahedron(b) do
-    gr = 1 / 2 * (1 + :math.sqrt(5))
+    gr = 1 / 2 * (1 + sqrt(5))
 
     ce =
-      :math.pow(gr / 2 + :math.sqrt(gr - 5 / 27) / 2, 1 / 3) +
-        :math.pow(gr / 2 - :math.sqrt(gr - 5 / 27) / 2, 1 / 3)
+      pow(gr / 2 + sqrt(gr - 5 / 27) / 2, 1 / 3) +
+        pow(gr / 2 - sqrt(gr - 5 / 27) / 2, 1 / 3)
 
-    n = 12 * :math.pow(ce, 2) * (3 * gr + 1) - ce * (36 * gr + 7) - (53 * gr + 6)
-    d = 6 * :math.pow(:math.sqrt(3 - :math.pow(ce, 2)), 3)
-    :math.pow(b, 3) * n / d
+    n = 12 * pow(ce, 2) * (3 * gr + 1) - ce * (36 * gr + 7) - (53 * gr + 6)
+    d = 6 * pow(sqrt(3 - pow(ce, 2)), 3)
+    pow(b, 3) * n / d
   end
 end
