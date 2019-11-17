8c06fdd9959d0349b029dc7763098e5fd6ad88f9
Alexa Hunleth <alexa@hunleth.com>
2019-11-17 16:20:08 -0500

    Added tests for the platonic solid surface area

--- test/polyhedra/surface_area_test.exs
+++ test/polyhedra/surface_area_test.exs
@@ -10,4 +10,16 @@ defmodule Polyhedra.SurfaceAreaTest do
   test "computes tetrahedron surface area" do
     assert round(SurfaceArea.tetrahedron(5)) == 43
   end
+
+  test "computes octahedron surface area" do
+    assert round(SurfaceArea.octahedron(5)) == 87
+  end
+
+  test "computes icosahedron surface area" do
+    assert round(SurfaceArea.icosahedron(5)) == 217
+  end
+
+  test "computes dodecahedron surface area" do
+    assert round(SurfaceArea.dodecahedron(5)) == 516
+  end
 end
